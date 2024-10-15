af764bc1ce304ad0dc750390d771aed225fed6d7
Marco Beduschi <8469440+marco-beduschi@users.noreply.github.com>
2024-10-15 08:32:51 -0300

    Refactor code to #*_ellapsed_minutes

--- app/models/calendar_event.rb
+++ app/models/calendar_event.rb
@@ -2,4 +2,12 @@ class CalendarEvent < ApplicationRecord
   belongs_to :calendar
 
   scope :week_of, ->(day) { where(starts_at: day.all_week) }
+
+  def starts_at_ellapsed_minutes
+    ( starts_at - starts_at.beginning_of_day ) / 60 / 60
+  end
+
+  def ends_at_ellapsed_minutes
+    ( ends_at - ends_at.beginning_of_day ) / 60 / 60
+  end
 end
